6dc8c099a22ed3ed8618f0e209b19d7e2354ce5d
Jerry <ypf201410@163.com>
2019-04-22 15:13:39 +0800

    商品超市

---
+++ src/com/day08/FruitItem.java
@@ -0,0 +1,23 @@
+package com.day08;
+/*
+    自定义类，描述商品信息
+    商品的属性
+        编号，int
+        品名，string
+        价格，double
+        数量，int
+        总价，double
+ */
+
+public class FruitItem {
+    //商品编号
+    int ID;
+    //商品名字
+    String name;
+    //商品单价
+    double price;
+    //商品数量
+    int number;
+    //总金额
+    double money;
+}
