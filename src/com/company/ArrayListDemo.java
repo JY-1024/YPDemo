3f2ff2465a9dab4fcb7958cadacee6171e40f2d3
Jerry <ypf201410@163.com>
2019-04-22 10:23:42 +0800

    集合、数组

--- src/com/company/ArrayListDemo.java
+++ src/com/company/ArrayListDemo.java
@@ -11,19 +11,36 @@ public class ArrayListDemo {
         array.add("yyyyy");
         array.add("ppppp");
         array.add("rrrrr");
+        //在索引2 位置添加 xiaoming
+        array.add(2,"xiaoming");
         int size = array.size();
         System.out.println(size);
 
-        String s = array.get(1);
+        //获取某个元素
+        String s = array.get(0);
         System.out.println(s);
+
+        System.out.println("==================================");
+
+        //set(int 索引,修改后的元素) 将指定索引的元素，进行修改
+        array.set(0,"88888");
 
         for (int i = 0; i < size ; i++) {
             System.out.println(array.get(i));
         }
 
-        //遍历
+        //删除指定索引位置的元素
+        array.remove(4);
+        System.out.println("=================================");
+        for (int i = 0; i < array.size() ; i++) {
+            System.out.println(array.get(i));
+        }
 
-
+        //clear() 清空元素，但集合还存在
+        array.clear();
+        for (int i = 0; i < array.size() ; i++) {
+            System.out.println(array.get(i));
+        }
 
 
     }
