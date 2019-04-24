fd1732666aed1afd06d02925f9d3231c4dfdd773
Jerry <ypf201410@163.com>
2019-04-25 07:04:54 +0800

    正则规则匹配

--- src/com/day16/demo01/RegexDemo.java
+++ src/com/day16/demo01/RegexDemo.java
@@ -13,4 +13,34 @@ package com.day16.demo01;
  *  "abc0123".replaceAll("[\\d]","#")
  */
 public class RegexDemo {
+    public static void main(String[] args) {
+        checkQQ();
+        checkTel();
+    }
+
+    /**
+     * 检查手机号码是否合法
+     * 1开头，可以是345678 0-9 位数固定11位
+     */
+    public static void checkTel(){
+        String telNumber = "13351258005";
+        boolean b = telNumber.matches("1[345678][\\d]{9}");
+        System.out.println(b);
+    }
+
+    /**
+     * 检查qq号码是否合法
+     * 0不能开头，全数字，位数5，10位
+     * 123456
+     * \\d
+     */
+    public static void checkQQ(){
+        String QQ = "123456";
+        //检查QQ号码和规则是否匹配，String累的方法matches
+        boolean b = QQ.matches("[1-9][\\d]{4,9}");
+//        boolean b = QQ.matches("[1-9][0-9]{4,9}");
+        System.out.println(b);
+    }
+
+
 }
