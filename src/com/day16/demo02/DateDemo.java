ac51c46a2d6e5386c3ee15a7641adbe7bae88b34
Jerry <ypf201410@163.com>
2019-04-25 11:03:11 +0800

    毫秒值概念

---
+++ src/com/day16/demo02/DateDemo.java
@@ -0,0 +1,21 @@
+package com.day16.demo02;
+
+/**
+ * 时间和日期类
+ *  java.util.Date
+ *
+ *  毫秒概念：1000毫秒=1秒
+ *
+ *  毫秒的0点：
+ *  System.currentTimeMillis() 返回值long类型参数
+ *  获取当前日期的毫秒值  1556161269563
+ *  时间原点；公元1970年1月1日，午夜0:00:00  毫秒值就是0
+ *  现在时间是 2088年8月8日
+ */
+public class DateDemo {
+    public static void main(String[] args) {
+       long time = System.currentTimeMillis();
+        System.out.println(time);
+    }
+
+}
