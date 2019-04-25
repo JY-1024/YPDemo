1e5f06d988284b97fec9c3e21857218fd301fc0b
Jerry <ypf201410@163.com>
2019-04-25 13:04:31 +0800

    Calendar中的add方法,Calendar转Date类型

--- src/com/day16/demo03/CalendarDemo1.java
+++ src/com/day16/demo03/CalendarDemo1.java
@@ -1,11 +1,43 @@
 package com.day16.demo03;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarDemo1 {
     public static void main(String[] args) {
 //        function();
-        function_2();
+//        function_2();
+//        function_3();
+        function_4();
+    }
+
+    /**
+     * Calendar类的方法getTime()
+     * 把日历对象，转成Date对象
+     */
+    public static void function_4(){
+        Calendar c = Calendar.getInstance();
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+
+    /**
+     * Calendar类方法add
+     * 日历的偏移量，可以指定一个日历中的字段，进行整数的偏移
+     * add(int field,int value)
+     */
+    public static void function_3(){
+        Calendar c = Calendar.getInstance();
+        //让日历中的天数，向后偏移280天
+        c.add(Calendar.YEAR,280);
+
+        //获取年份
+        int year = c.get(Calendar.YEAR);
+        //获取月份
+        int month = c.get(Calendar.MONTH)+1;
+        //获取天数
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        System.out.println(year + "年"+month+"月"+day+"日");
     }
 
     /**
