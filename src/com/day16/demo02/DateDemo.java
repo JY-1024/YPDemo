ba8e3bb24a197a181d18dddd5ed98aa47f2286c9
Jerry <ypf201410@163.com>
2019-04-25 11:04:19 +0800

    毫秒值概念

--- src/com/day16/demo02/DateDemo.java
+++ src/com/day16/demo02/DateDemo.java
@@ -11,6 +11,9 @@ package com.day16.demo02;
  *  获取当前日期的毫秒值  1556161269563
  *  时间原点；公元1970年1月1日，午夜0:00:00  毫秒值就是0
  *  现在时间是 2088年8月8日
+ *
+ *  重要：时间和日期的计算，必须依赖毫秒值
+ *
  */
 public class DateDemo {
     public static void main(String[] args) {
