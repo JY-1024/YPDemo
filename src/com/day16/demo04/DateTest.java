4cfcca25ec673cd61345ea7cb8487473795e1e99
Jerry <ypf201410@163.com>
2019-04-25 13:26:50 +0800

    计算闰年方法

--- src/com/day16/demo04/DateTest.java
+++ src/com/day16/demo04/DateTest.java
@@ -1,20 +1,36 @@
 package com.day16.demo04;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
-/**
- * 计算活了多少天
- * 生日  今天的日期
- * 两个日期，变成毫秒值，做减法
- *
- */
+
 public class DateTest {
     public static void main(String[] args) throws Exception {
-        function();
+//        function();
+        function_1();
     }
 
+    /**
+     * 闰年计算
+     * 2000 3000
+     * 高级算法：日历设置到指定年份的3月1日，add向前偏移1天，获取天数，如果是29天是闰年,28天是平年
+     */
+    public static void function_1(){
+        Calendar c = Calendar.getInstance();
+        c.set(2000,2,1);
+        c.add(Calendar.DAY_OF_MONTH,-1);
+        //get方法获取天数
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        System.out.println(day);
+    }
+    /**
+     * 计算活了多少天
+     * 生日  今天的日期
+     * 两个日期，变成毫秒值，做减法
+     *
+     */
     public static void function() throws Exception {
         System.out.println("请输入出生日期 格式YYYY-MM-dd");
         //获取出生日期，键盘输入
